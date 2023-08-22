3568b67a3031d0603c83882806d8ee0270364317
TerryEHLee <terryehlee@gmail.com>
2023-08-22 19:08:25 +0900

    for pull

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { useRouter } from "next/navigation";
+import { supabase } from "@/libs/supabase";
 
 const Header = () => {
   const router = useRouter();
