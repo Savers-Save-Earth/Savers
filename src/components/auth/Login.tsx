2ad4b073ef8a255ecc8797b115ae097f96337889
TerryEHLee <terryehlee@gmail.com>
2023-08-22 15:55:30 +0900

    Feat/Header header 각 항목 클릭 시 해당 항목으로 이동 회원가입 버튼 추가

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,7 +1,25 @@
 "use client";
 import React, { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { supabae } from "../../libs/supabase";
+
+interface FormValue {
+  email: string;
+  password: string;
+  confirmingPw: string;
+}
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormValue>();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return <div>Login</div>;
 };
 
