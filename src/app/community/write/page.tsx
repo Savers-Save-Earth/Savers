0e75415bfa22d439c13dc3df6233c51f170a73f2
Jiwon Park <xoxojw.dev@gmail.com>
2023-08-22 17:20:28 +0900

    design/ 게시글 작성 에디터 ui 구현

--- src/app/community/write/page.tsx
+++ src/app/community/write/page.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from "next";
 import AddPost from "@/components/community/AddPost";
 
+// 세션 여부 확인 후 로그인 유저 없으면 메인으로 이동
+
 const Write: NextPage = () => {
   return (
     <>
