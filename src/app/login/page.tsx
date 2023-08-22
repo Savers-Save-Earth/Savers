418c8b6e9549a31cb1ce4693abddd73cb1c52c42
[Mia Lee] <korea_stan@naver.com>
2023-08-22 18:40:25 +0900

    for merge

---
+++ src/app/login/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "@/components/Header";
+import Login from "@/components/auth/Login";
+
+const login = () => {
+  return (
+    <>
+      <Header />
+      <Login />
+    </>
+  );
+};
+
+export default login;
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "@/components/Header";
+
+const signup = () => {
+  return (
+    <>
+      <Header />
+      <div>signup</div>
+    </>
+  );
+};
+
+export default signup;
