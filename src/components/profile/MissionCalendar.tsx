9f246a7bafa7e01138ae4724a94d0ba2954d65a8
[Mia Lee] <korea_stan@naver.com>
2023-08-27 15:18:32 +0900

    for merge

--- src/components/profile/MissionCalendar.tsx
+++ src/components/profile/MissionCalendar.tsx
@@ -1,13 +1,13 @@
-"use client";
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css"; // css import
-import { format } from "date-fns"; // date-fns의 format 함수를 import
+import "react-calendar/dist/Calendar.css";
+import { format } from "date-fns";
 
 type ValuePiece = Date | null;
 
 type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+const marks = ["2023-08-24"];
 
 const MissionCalendar = () => {
   const [value, onChange] = useState<Value>(new Date());
@@ -17,11 +17,18 @@ const MissionCalendar = () => {
       <Calendar
         onChange={onChange}
         value={value}
-        minDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
-        maxDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
+        minDetail="month"
+        maxDetail="month"
         showNeighboringMonth={false}
+        locale="ko-KO"
         className="mx-auto w-full text-sm border-b"
-        formatDay={(locale, date) => format(date, "d")}
+        formatDay={(locale, date) => format(date, "dd")}
+        tileClassName={({ date, view }) => {
+          const formattedDate = format(date, "yyyy-MM-dd");
+          if (marks.includes(formattedDate)) {
+            return "highlight";
+          }
+        }}
       />
     </div>
   );
