eba65c2200fddc8485dad719c32019964d18a842
[Mia Lee] <korea_stan@naver.com>
2023-08-27 14:14:34 +0900

    feat/mypage - [좋아요] 뱃지

--- src/components/profile/Badges.tsx
+++ src/components/profile/Badges.tsx
@@ -42,22 +42,36 @@ const Badges = () => {
   const threeShareBadge =
     badges.filter((item) => item.badge_title === "share").length >= 3;
 
+  const likeBadge = badges.find((item) => item.badge_title === "like");
+  const threeLikeBadge =
+    badges.filter((item) => item.badge_title === "like").length >= 3;
+
   useEffect(() => {
     fetchUser();
   }, []);
 
   return (
     <div>
-      <p>회원가입시 받는 배지</p>
+      <p>[새싹 세이버] 회원가입시 받는 배지</p>
       <p>
         {shareBadge
-          ? "공유하기 배지"
+          ? "[나눔꾼] 물건 공유하기 배지"
           : "아직 공유하기 배지를 획득하지 못했습니다."}
       </p>
       <p>
         {threeShareBadge
-          ? "세번 공유하기 배지"
+          ? "[나눔꾼3] 물건 세번 공유하기 배지"
           : "아직 세번 공유하기 배지를 획득하지 못했습니다."}
+      </p>
+      <p>
+        {likeBadge
+          ? "[사랑꾼] 좋아요 1회 배지"
+          : "좋아요 배지를 획득하지 못했습니다."}
+      </p>
+      <p>
+        {threeLikeBadge
+          ? "[사랑꾼] 좋아요 3회 배지"
+          : "좋아요 3회 배지를 획득하지 못했습니다."}
       </p>
     </div>
   );
