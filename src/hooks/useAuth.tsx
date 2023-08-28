4b7815073f9af30f46460b075ad470795898c52a
Jiwon Park <xoxojw.dev@gmail.com>
2023-08-28 10:52:14 +0900

    #63 feat/ useAuth 훅 추가

---
+++ src/hooks/useAuth.tsx
@@ -0,0 +1,49 @@
+import supabase from "@/libs/supabase";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+
+interface currentUserType {
+  uid: string;
+  nickname: string;
+  email: string;
+  profileImage: string;
+}
+
+const useAuth = (): currentUserType | null => {
+  const [currentUser, setCurrentUser] = useState<currentUserType | null>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchAuthData = async () => {
+      try {
+        const { data: { session } } = await supabase.auth.getSession();
+        if (session === null) {
+          setCurrentUser(null);
+        } else {
+          const userUid = session.user.id;
+          const { data: users, error } = await supabase
+            .from("user")
+            .select("uid, nickname, email, profileImage")
+            .eq("uid", userUid)
+            .single();
+
+          if (error) {
+            console.error("사용자 정보 로딩 에러 >> ", error);
+            setCurrentUser(null);
+          } else {
+            setCurrentUser(users);
+          }
+        }
+      } catch (error) {
+        console.error("세션 로딩 에러 >> ", error);
+        setCurrentUser(null);
+      }
+    };
+
+    fetchAuthData();
+  }, []);
+
+  return currentUser;
+};
+
+export default useAuth;
