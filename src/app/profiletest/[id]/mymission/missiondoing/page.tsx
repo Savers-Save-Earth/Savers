0f621cec675c96aa92cf769fb67c9d45ad66d1d2
[Mia Lee] <korea_stan@naver.com>
2023-08-28 15:24:03 +0900

    design/캘린더 css

--- src/app/profiletest/[id]/mymission/missiondoing/page.tsx
+++ src/app/profiletest/[id]/mymission/missiondoing/page.tsx
@@ -3,6 +3,8 @@ import { convertDate } from "@/libs/util";
 import { ListMission } from "@/types/types";
 import React from "react";
 
+export const revalidate = 0;
+
 export const currentDate = convertDate(new Date());
 
 const MissionDoing = async ({ params }: { params: { id: string } }) => {
@@ -13,15 +15,16 @@ const MissionDoing = async ({ params }: { params: { id: string } }) => {
     .eq("createdAt", currentDate)
     .eq("userId", searchId)
     .eq("doingYn", true);
+
   if (error) {
     console.log("데이터가져올 때 에러남");
     return false;
   }
-  if (dailyMission!.length === 0) return <div>일일미션을 받아주세요!</div>;
+  if (dailyMission?.length === 0) return <div>일일미션을 받아주세요!</div>;
 
   return (
     <div className="bg-green-200 h-full flex justify-center items-center gap-x-16 text-white">
-      {dailyMission!.map((mission: ListMission) => {
+      {dailyMission?.map((mission: ListMission) => {
         return (
           <div className="bg-slate-500" key={mission.id}>
             <p>{mission.id}</p>
