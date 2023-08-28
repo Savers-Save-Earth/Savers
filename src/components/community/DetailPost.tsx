c799b0b9aca4d915c41b3806b3f8285810fc2aca
Jiwon Park <xoxojw.dev@gmail.com>
2023-08-28 16:49:35 +0900

    #63 design/ 좋아요 버튼을 북마크 버튼으로 변경

--- src/components/community/DetailPost.tsx
+++ src/components/community/DetailPost.tsx
@@ -67,26 +67,22 @@ const DetailPost = () => {
             <h1 className="text-3xl text-gray-700 font-semibold">
               {postDetail?.title}
             </h1>
-              {currentUser?.uid === postDetail?.author_uid ?
-                (
-                  <div className="flex space-x-3">
-                    <button
-                      onClick={handleEditClick}
-                      className="w-20 text-sm border-b-4 border-blue-300 px-5 pb-1 shadow-sm hover:-translate-y-1 transition ease-in-out duration-200"
-                    >
-                      수정
-                    </button>
-                    <button
-                      onClick={handleDelete}
-                      className="w-20 text-sm border-b-4 border-blue-300 px-5 pb-1 shadow-sm hover:-translate-y-1 transition ease-in-out duration-200"
-                    >
-                      삭제
-                    </button>
-                  </div>
-                ) : (
-                  null
-                )
-            }
+            {currentUser?.uid === postDetail?.author_uid ? (
+              <div className="flex space-x-3">
+                <button
+                  onClick={handleEditClick}
+                  className="w-20 text-sm border-b-4 border-blue-300 px-5 pb-1 shadow-sm hover:-translate-y-1 transition ease-in-out duration-200"
+                >
+                  수정
+                </button>
+                <button
+                  onClick={handleDelete}
+                  className="w-20 text-sm border-b-4 border-blue-300 px-5 pb-1 shadow-sm hover:-translate-y-1 transition ease-in-out duration-200"
+                >
+                  삭제
+                </button>
+              </div>
+            ) : null}
           </div>
           <div className="flex justify-between mt-3 py-3">
             <div className="flex space-x-5 items-center justify-center">
@@ -103,21 +99,38 @@ const DetailPost = () => {
               dangerouslySetInnerHTML={{ __html: postDetail.content }}
               className="mt-10"
             />
-            )}
+          )}
           <div className="mt-5 mx-auto space-x-5">
             <button>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+                />
               </svg>
             </button>
             <button>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
-            </svg>
-            </button>
-            <button>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
+                />
               </svg>
             </button>
           </div>
--- src/components/community/GetPosts.tsx
+++ src/components/community/GetPosts.tsx
@@ -18,22 +18,27 @@ type QueryKeyMap = {
 
 const GetPosts = () => {
   const pathname = usePathname();
-  const getPathnameQueryKey = (pathname: string) => {
-  const queryKeyMap: QueryKeyMap = {
-    "/community": ["allPosts"],
-    "/community/product": ["productPosts"],
-    "/community/restaurant": ["restaurantPosts"],
-    "/community/recipe": ["recipePosts"],
-    "/community/ohjiwan": ["ohjiwanPosts"],
-  };
 
-  return queryKeyMap[pathname] || ["allPosts"];
+  // 현재 pathname에 따라 쿼리키 설정하는 함수
+  const getPathnameQueryKey = (pathname: string) => {
+    const queryKeyMap: QueryKeyMap = {
+      "/community": ["allPosts"],
+      "/community/product": ["productPosts"],
+      "/community/restaurant": ["restaurantPosts"],
+      "/community/recipe": ["recipePosts"],
+      "/community/ohjiwan": ["ohjiwanPosts"],
+    };
+
+    return queryKeyMap[pathname] || ["allPosts"];
   };
+  // queryKey 변수에 쿼리키 설정함수의 return값 할당
   const queryKey = getPathnameQueryKey(pathname);
 
-  const { isLoading, data: posts, error } = useQuery<PostType[]>(
-    queryKey,
-    () => getPosts(pathname), {
+  const {
+    isLoading,
+    data: posts,
+    error,
+  } = useQuery<PostType[]>(queryKey, () => getPosts(pathname), {
     staleTime: 60000,
     cacheTime: 300000,
   });
@@ -47,9 +52,7 @@ const GetPosts = () => {
     <>
       {
         <div className="flex flex-col max-w-7xl my-10 mx-auto items-center justify-center">
-          <h1 className="text-xl flex mx-auto my-10">
-            전체 글
-          </h1>
+          <h1 className="text-xl flex mx-auto my-10">전체 글</h1>
           {Array.isArray(posts) &&
             posts.map((post: PostType) => (
               <div
@@ -75,13 +78,45 @@ const GetPosts = () => {
                         </p>
                       </Link>
                     </div>
-                    <div className="flex justify-between text-sm text-gray-500 mt-5">
-                      <div className="space-x-2">
-                        <span>댓글 {getCommentsNum(post.post_uid)}</span>
-                        <span>좋아요 0</span>
+                    <div className="flex justify-between mt-5">
+                      <div className="flex space-x-3">
+                        <div className="space-x-1 items-center justify-center flex text-sm text-gray-500">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            className="w-5 h-5"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z"
+                            />
+                          </svg>
+                          <span>{getCommentsNum(post.post_uid)}</span>
+                        </div>
+                        <div className="space-x-1 items-center justify-center flex text-sm text-gray-500">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+                            />
+                          </svg>
+                          <span>0</span>
+                        </div>
                       </div>
                       <span className="text-sm text-gray-400">
-                        {post.updated_date}
+                        {post.updated_date.split(" ")[0]}
                       </span>
                     </div>
                   </div>
