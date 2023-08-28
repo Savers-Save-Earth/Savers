6855b49f7265c3c5c300b06240b9673faf0242f9
[Mia Lee] <korea_stan@naver.com>
2023-08-28 21:23:42 +0900

    feat/main - 북마크 된 레스토랑 추가함

--- src/components/main/RestaurantList.tsx
+++ src/components/main/RestaurantList.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+"use client";
+
+import supabase from "@/libs/supabase";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 const RestaurantList = () => {
-  return <div>RestaurantList</div>;
+  const [restaurantList, setRestaurantList] = useState<any[]>([]);
+  const fetchRestaurant = async () => {
+    const { data } = await supabase.from("like_restaurant").select();
+
+    setRestaurantList(data);
+  };
+
+  useEffect(() => {
+    fetchRestaurant();
+  }, []);
+  return (
+    <div className="p-24 items-start gap-16 self-stretch">
+      <h1 className="text-2xl">세이버 픽 레스토랑</h1>
+      {restaurantList.map((item) => (
+        <div
+          key={item.id}
+          className="rounded-lg border border-gray-200 bg-white p-4 mt-5"
+        >
+          <p>{item.restaurant_category}</p>
+          <p>{item.restaurant_name}</p>
+          <p>{item.restaurant_address}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default RestaurantList;
