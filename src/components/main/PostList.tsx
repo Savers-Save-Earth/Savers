7af4dd8470158a8e6260a8b04d7b8741d419ac01
[Mia Lee] <korea_stan@naver.com>
2023-08-29 19:53:00 +0900

    main/좋아요 순으로 인기글 정렬

--- src/components/main/PostList.tsx
+++ src/components/main/PostList.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import { useState, useEffect } from "react";
 import supabase from "@/libs/supabase";
 import { Post } from "@/types/types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookmark } from "@fortawesome/free-regular-svg-icons";
 
 const PostList = () => {
   const [post, setPost] = useState<Post[]>([]);
@@ -11,7 +13,24 @@ const PostList = () => {
   const fetchPost = async () => {
     try {
       const { data } = await supabase.from("community").select();
-      setPost(data || []);
+
+      const { data: likeCount } = await supabase.from("like_post").select();
+      if (data) {
+        // 북마크 숫자 세는 로직
+        const updatedData = data.map((item) => ({
+          ...item,
+          like_count: likeCount?.filter((i) => i.post_uid === item.post_uid)
+            .length,
+        }));
+
+        // 북마크 숫자대로 정렬
+        const sortedData = updatedData.sort(
+          (a, b) => b.like_count - a.like_count,
+        );
+
+        setPost(sortedData);
+        console.log(sortedData);
+      }
     } catch (error) {
       console.error("Error fetching products:", error);
     }
@@ -23,6 +42,8 @@ const PostList = () => {
   const showMorePost = () => {
     setShowCount(showCount + 3);
   };
+
+  const sortedPost = post.sort((a, b) => b.created_date - a.created_date);
 
   return (
     <div className="p-24 items-start gap-16 self-stretch">
@@ -39,7 +60,15 @@ const PostList = () => {
               ? `${item.content.replace(/<[^>]*>/g, "").slice(0, 20)}...`
               : item.content.replace(/<[^>]*>/g, "")}
           </p>
-          <p className="mt-2 text-gray-500">{item.created_date}</p>
+          <span className="mt-2 text-gray-500">{item.created_date}</span>
+          <span className="text-sm ml-2">
+            <FontAwesomeIcon
+              icon={faBookmark}
+              size="xs"
+              style={{ color: "#000000", marginRight: "5px" }}
+            />
+            {item.like_count}
+          </span>
         </div>
       ))}
       <button onClick={showMorePost}>
