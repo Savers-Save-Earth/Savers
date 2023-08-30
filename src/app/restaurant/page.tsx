15cbba8cb3e6d851895d4b7887957be9c86da773
Jiwon Park <xoxojw.dev@gmail.com>
2023-08-30 22:46:31 +0900

    fix/ kakao window 오류 발생 부분 주석처리

--- src/app/restaurant/page.tsx
+++ src/app/restaurant/page.tsx
@@ -6,11 +6,14 @@ import KakaoMap from "../utils/kakaoMap";
 import { useEffect } from "react";
 
 const Restaurant = () => {
-  useEffect(() => {
-    if (!kakao.isInitialized()) {
-      Kakao.init(process.env.NEXT_PUBLIC_KAKAOMAP_KEY);
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (typeof window !== "undefined") {
+  //     const { Kakao } = window;
+
+  //     if (!Kakao.isInitialized()) {
+  //       // SDK 초기화 부분, 본인의 API KEY 입력
+  //       Kakao.init(process.env.NEXT_PUBLIC_KAKAO_API_KEY);
+  //     }
   return (
     <>
       <KakaoMap />
