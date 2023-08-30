a076c260dffa242cc7c45056b7b2ec39f2e6b873
Jiwon Park <xoxojw.dev@gmail.com>
2023-08-31 01:56:30 +0900

    #110 fix/ 인기글 불필요한 페이지에서 제외

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,10 +1,8 @@
 import GetPosts from "@/components/community/GetPosts";
-import PopularPosts from "@/components/community/PopularPosts";
 
 const Community = () => {
   return (
     <>
-      <PopularPosts />
       <GetPosts />
     </>
   );
