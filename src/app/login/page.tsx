8f844283295e25e54f71256b4a6b40a08f93c954
TerryEHLee <terryehlee@gmail.com>
2023-08-30 16:40:03 +0900

    for pull

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,6 +6,9 @@ import PwLogin from "@/components/auth/PwLogin";
 const login = () => {
   return (
     <>
+      <div className="text-gray-900 font-Pretendard text-24 font-semibold">
+        로그인/회원가입
+      </div>
       <PwLogin />
       <SocialLogin />
     </>
