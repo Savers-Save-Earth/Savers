523fe8d045812d19af520ce57be11adf50b90cea
TerryEHLee <terryehlee@gmail.com>
2023-08-31 14:03:55 +0900

    for pull

--- src/app/profile/[id]/mymission/missiondoing/page.tsx
+++ src/app/profile/[id]/mymission/missiondoing/page.tsx
@@ -1,38 +1,105 @@
+'use client'
+
 import supabase from "@/libs/supabase";
 import { convertDate } from "@/libs/util";
+import { Database } from "@/types/supabase";
 import { ListMission } from "@/types/types";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const MissionDoing = async ({ params }: { params: { id: string; }; }) => {
-  const currentDate = convertDate(new Date());
+// export const revalidate = 0
+type MissionDoingProp =Database["public"]["Tables"]["missionList"]["Row"];
+
+
+const MissionDoing = ({ params }: { params: { id: string } }) => {
+   const currentDate = convertDate(new Date());
+
+  const [dailyMission, setDailyMission] = useState<MissionDoingProp[]>([])
+  const [isLoading, setIsLoading] = useState<boolean>(true); // 로딩 상태 추가
   const searchId = decodeURIComponent(params.id);
-  let { data: dailyMission, error } = await supabase
-    .from("missionList")
-    .select("*")
-    .eq("createdAt", currentDate)
-    .eq("userId", searchId)
-    .eq("doingYn", true);
 
-  if (error) {
-    console.log("데이터가져올 때 에러남");
+  useEffect(() => {
+    fetchMissionData(searchId)
+  },[])
+
+  const fetchMissionData = async (searchId: string) => {
+    try {
+      let { data: dailyMission } = await supabase
+      .from("missionList")
+      .select("*")
+      .eq("createdAt", currentDate)
+      .eq("userId", searchId)
+      .eq("doingYn", true);
+      setIsLoading(false)
+      if (dailyMission?.length === 0) return <div>일일미션을 받아주세요!</div>;
+      else {
+        setDailyMission(dailyMission)
+      }
+    } catch (error) {
+      console.log("데이터가져올 때 에러남");
+      setIsLoading(false);
     return false;
-  }
-  if (dailyMission?.length === 0) return <div>일일미션을 받아주세요!</div>;
+    }
+  } 
 
   return (
-    <div className="bg-green-200 h-full flex justify-center items-center gap-x-16 text-white">
-      {dailyMission?.map((mission: ListMission) => {
-        return (
-          <div className="bg-slate-500" key={mission.id}>
-            <p>{mission.id}</p>
-            <p>{mission.title}</p>
-            <p>{mission.content}</p>
-            <p>{mission.doingYn.toString()}</p>
-          </div>
-        );
-      })}
-    </div>
+    <>
+
+        <div className="bg-green-200 h-full flex justify-center items-center gap-x-16 text-white">
+          {dailyMission?.map((mission) => {
+            return (
+              <div className="bg-slate-500" key={mission.id}>
+                <p>{mission.id}</p>
+                <p>{mission.title}</p>
+                <p>{mission.content}</p>
+                <p>{mission.doingYn!.toString()}</p>
+              </div>
+            );
+          })}
+        </div>
+    </>
   );
 };
 
 export default MissionDoing;
+
+// import supabase from "@/libs/supabase";
+// import { convertDate } from "@/libs/util";
+// import { ListMission } from "@/types/types";
+// import React from "react";
+
+// // export const revalidate = 0
+
+// export const currentDate = convertDate(new Date());
+
+// const MissionDoing = async ({ params }: { params: { id: string } }) => {
+//   const searchId = decodeURIComponent(params.id);
+//   let { data: dailyMission, error } = await supabase
+//     .from("missionList")
+//     .select("*")
+//     .eq("createdAt", currentDate)
+//     .eq("userId", searchId)
+//     .eq("doingYn", true);
+
+//   if (error) {
+//     console.log("데이터가져올 때 에러남");
+//     return false;
+//   }
+//   if (dailyMission?.length === 0) return <div>일일미션을 받아주세요!</div>;
+
+//   return (
+//     <div className="bg-green-200 h-full flex justify-center items-center gap-x-16 text-white">
+//       {dailyMission?.map((mission: ListMission) => {
+//         return (
+//           <div className="bg-slate-500" key={mission.id}>
+//             <p>{mission.id}</p>
+//             <p>{mission.title}</p>
+//             <p>{mission.content}</p>
+//             <p>{mission.doingYn.toString()}</p>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// };
+
+// export default MissionDoing;
--- src/app/profile/[id]/mymission/missiondone/page.tsx
+++ src/app/profile/[id]/mymission/missiondone/page.tsx
@@ -1,39 +1,63 @@
+'use client'
+
 import supabase from "@/libs/supabase";
 import { convertDate } from "@/libs/util";
 import { ListMission } from "@/types/types";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const MissionDone = async ({ params }: { params: { id: string } }) => {
-  const currentDate = convertDate(new Date());  
+// export const revalidate = 0;
+
+export const currentDate = convertDate(new Date());
+
+const MissionDone = ({ params }: { params: { id: string } }) => {
+  const [missionDone, setMissionDone] = useState([])
+  const [isLoading, setIsLoading] = useState<boolean>(true); // 로딩 상태 추가
   const searchId = decodeURIComponent(params.id);
-  let { data: dailyMission, error } = await supabase
+  useEffect(() => {
+    fetchMissionData(searchId)
+  },[])
 
-  .from("missionList")
-  .select("*")
-  .eq("userId", searchId)
-  .eq("doingYn", false)
-  if (error) {
-    console.log("데이터가져올 때 에러남");
+  const fetchMissionData = async (searchId: string) => {
+    try {
+      let { data: missionDone } = await supabase
+      .from("missionList")
+      .select("*")
+      // .eq("createdAt", currentDate)
+      .eq("userId", searchId)
+      .eq("doingYn", false);
+      setIsLoading(false); // 데이터 가져오기 후 로딩 상태를 false로 설정
+      if (missionDone?.length === 0) return <div>지금까지 완료한 미션이 없네요!!</div>;
+      else {
+        setMissionDone(missionDone)
+      }
+    } catch (error) {
+      console.log("데이터가져올 때 에러남");
+      setIsLoading(false);
     return false;
+    }
   }
-if (dailyMission!.length === 0) return (
-  <div>지금까지 완료한 미션이 없네요!!</div>
-)
 
   return (
-    <div className="bg-green-200 h-full justify-center items-center gap-x-16 text-white">
-      {dailyMission!.map((mission: ListMission) => {
-        return (
-          <div className="bg-slate-500 mb-5" key={mission.id}>
-            <p>{mission.id}</p>
-            <p>미션 제목 : {mission.title}</p>
-            <p>미션 내용 : {mission.content}</p>
-            <p>미션 생성일 : {mission.createdAt}</p>
-            <p>미션 수행중인가요? : {mission.doingYn.toString()}</p>
-          </div>
-        );
-      })}
-    </div>
+    <>
+    {isLoading ? ( // isLoading이 true이면 로딩 표시를 표시합니다.
+        <p>Loading...</p>
+      ) : (
+        <div className="bg-green-200 h-full justify-center items-center gap-x-16 text-white">
+        {missionDone!.map((mission: ListMission) => {
+          return (
+            <div className="bg-slate-500 mb-5" key={mission.id}>
+              <p>{mission.id}</p>
+              <p>미션 제목 : {mission.title}</p>
+              <p>미션 내용 : {mission.content}</p>
+              <p>미션 생성일 : {mission.createdAt}</p>
+              <p>미션 수행중인가요? : {mission.doingYn.toString()}</p>
+            </div>
+          );
+        })}
+      </div>
+      )}
+    </>
+
   );
 };
 
