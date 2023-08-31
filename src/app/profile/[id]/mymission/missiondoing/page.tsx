6e495ab3068e576e4c92da8725536613aaff8e33
podoDJ <ourstar1345@gmail.com>
2023-08-31 20:27:24 +0900

    sidebar height fix

--- src/app/profile/[id]/mymission/missiondoing/page.tsx
+++ src/app/profile/[id]/mymission/missiondoing/page.tsx
@@ -55,12 +55,12 @@ const MissionDoing = ({ params }: { params: { id: string } }) => {
           {dailyMission?.map((mission) => {
             return (
               <div
-                className=" py-[40px] relative flex flex-col justify-start items-center bg-[#DBF8D9] w-[169px] h-[280px] rounded-2xl break-words gap-[40px] p-1"
+                className="py-[40px] px-3 flex flex-col justify-start items-center bg-[#DBF8D9] w-[169px] h-[280px] rounded-2xl break-words gap-[40px] p-1"
                 key={mission.id}
               >
                 <h1 className="text-center font-semibold">{mission.title}</h1>
-                <div>{mission.content}</div>
-                <button className="absolute bottom-8 py-1 px-3 rounded-full border-2 border-[#42723e] text-[#42723e] font-semibold hover:bg-[#42723e] hover:text-white hover:duration-500"
+                <div className="flex-grow">{mission.content}</div>
+                <button className="py-1 px-3 rounded-full border-2 border-[#42723e] text-[#42723e] font-semibold hover:bg-[#42723e] hover:text-white hover:duration-500"
                 onClick={() => mission.bigCategory === "글쓰기" ? window.open("/community") : window.open("/product")}
                 >
                   미션하러 가기
