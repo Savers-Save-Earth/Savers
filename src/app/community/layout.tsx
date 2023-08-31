e0bbecbc9d080ddcc3f15357e840884c4ad1be53
Jiwon Park <xoxojw.dev@gmail.com>
2023-08-31 19:47:27 +0900

    #110 fix/ 글쓰기 플로팅 버튼 렌더링 되지 않는 페이지 추가(글 작성, 수정 페이지)

--- src/app/community/layout.tsx
+++ src/app/community/layout.tsx
@@ -37,24 +37,29 @@ const CommunityLayout = ({ children }: { children: React.ReactNode }) => {
         }
         {children}
       </main>
-      {user ? (
-        <FloatingButton href="/community/write">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
-            />
-          </svg>
-        </FloatingButton>
-      ) : null}
+      {!user || pathname === "/community/write" || pathname === "/community/edit"
+        ? 
+        null
+        :
+        (
+          <FloatingButton href="/community/write">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+              />
+            </svg>
+          </FloatingButton>
+        )
+        }
     </div>
   );
 };
