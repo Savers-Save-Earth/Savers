958ed71ce8302dca0eb576ec44f4db6815ec8c34
TerryEHLee <terryehlee@gmail.com>
2023-09-08 21:57:46 +0900

    footer css ing

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,16 +7,16 @@ import Footer from "@/components/Footer";
 const login = () => {
   return (
     <>
-      <div className="flex w-full flex-col items-start gap-10 pt-28">
+      <div className="flex w-full flex-col items-start gap-10 pt-28 ">
         <div className="text-2xl pb-16 text-gray-900 font-semibold">
           로그인/회원가입
         </div>
-        <div className="flex flex-col mx-auto items-center justify-center">
+        {/* <div className="flex flex-col mx-auto items-center justify-center">
           <PwLogin />
           <SocialLogin />
-        </div>
-        <Footer />
+        </div> */}
       </div>
+      <Footer />
     </>
   );
 };
