1d54c7ee82d690887162fe06b4537fa6585669fd
TerryEHLee <terryehlee@gmail.com>
2023-09-09 06:56:06 +0900

    Design/Login-mobile

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,33 @@
+"use client";
 import React from "react";
 import SocialLogin from "@/components/auth/SocialLogin";
 import NicknameMaker from "@/components/auth/NicknameMaker";
 import PwLogin from "@/components/auth/PwLogin";
 import Footer from "@/components/Footer";
+import { useIsMobile } from "@/hooks/useIsMobile";
 
 const login = () => {
+  const isMobile = useIsMobile();
   return (
     <>
-      <div className="flex w-full flex-col items-start gap-10 pt-28 ">
-        <div className="text-2xl pb-16 text-gray-900 font-semibold">
-          로그인/회원가입
+      {isMobile ? (
+        <div className="flex w-full flex-col items-start gap-10 pt-28 ">
+          <div className="flex flex-col mx-auto items-center justify-center">
+            <PwLogin />
+            <SocialLogin />
+          </div>
         </div>
-        <div className="flex flex-col mx-auto items-center justify-center">
-          <PwLogin />
-          <SocialLogin />
+      ) : (
+        <div className="flex w-full flex-col items-start gap-10 pt-28 ">
+          <div className="text-2xl pb-16 text-gray-900 font-semibold">
+            로그인/회원가입
+          </div>
+          <div className="flex flex-col mx-auto items-center justify-center">
+            <PwLogin />
+            <SocialLogin />
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
