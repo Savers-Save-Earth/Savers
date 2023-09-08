3b52e08c6e2ecc3d21c8f2a8083d03aa45c521ee
Jiwon Park <xoxojw.dev@gmail.com>
2023-09-08 19:48:06 +0900

    feat/ 세션 감지하여 비로그인 진입 시 리다이렉트, 새로고침/뒤로가기 이벤트 방지 #163

--- src/app/community/write/page.tsx
+++ src/app/community/write/page.tsx
@@ -11,18 +11,62 @@ const Write: NextPage = () => {
 
   useEffect(() => {
     const getSessionState = async () => {
-      const { data: session, error } = await supabase.auth.getSession();
-      if (!session.session) router.push("/");
+      const { data: session } = await supabase.auth.getSession();
       setSessionState(session.session);
-    }
+
+      if (!session.session) {
+        router.push("/");
+      }
+    };
+
     getSessionState();
+
+    // 로그아웃 이벤트 감지
+    const authListener = supabase.auth.onAuthStateChange((event, session) => {
+      if (event === "SIGNED_OUT") {
+        router.push("/");
+      }
+    });
+
+    return () => {
+      // 컴포넌트 언마운트시 리스너 제거
+      authListener.data.subscription.unsubscribe();
+    };
   }, [router]);
+
+  // 새로고침, 창 닫기 방지
+  const preventClose = (e: BeforeUnloadEvent) => {
+    e.preventDefault();
+    e.returnValue = "";
+  };
+
+  useEffect(() => {
+    window.addEventListener("beforeunload", preventClose);
+    return () => {
+      window.removeEventListener("beforeunload", preventClose);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // 뒤로가기 방지
+  useEffect(() => {
+    const preventGoBack = () => {
+      if (confirm("페이지를 나가시겠습니까?")) {
+        history.go(-1);
+      } else {
+        history.pushState(null, "", location.href);
+      }
+    };
+    history.pushState(null, "", location.href);
+    window.addEventListener("popstate", preventGoBack);
+    return () => window.removeEventListener("popstate", preventGoBack);
+  }, []);
 
   return (
     <>
       <AddPost />
     </>
-  )
-}
+  );
+};
 
 export default Write;
