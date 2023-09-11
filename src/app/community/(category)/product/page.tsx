75340262eeba5d65385ef1d023a47408962bf634
Jiwon Park <xoxojw.dev@gmail.com>
2023-09-11 18:19:14 +0900

    update/ metadata 업데이트 #163

--- src/app/community/(category)/product/page.tsx
+++ src/app/community/(category)/product/page.tsx
@@ -1,14 +1,13 @@
+import Seo from '@/components/Seo';
 import GetPosts from '@/components/community/posts/GetPosts';
 import { Metadata } from 'next';
 
-export const metadata: Metadata = {
-  title: "제품 | Savers",
-  description: "친환경 제품에 대한 이야기를 나눠보세요.",
-};
-
 const CommunityProduct = () => {
   return (
-    <GetPosts />
+    <>
+      <Seo title="제품 | Savers" description="친환경 제품에 대한 이야기를 나눠보세요." />
+      <GetPosts />
+    </>
   )
 }
 
