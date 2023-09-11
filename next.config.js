aa1c251043b395fa7dfa9fb523004197b5d9540d
podoDJ <ourstar1345@gmail.com>
2023-09-11 11:03:39 +0900

    fix/modify nextconfig for next-image next-video

--- next.config.js
+++ next.config.js
@@ -1,39 +1,79 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_SUPABASE_STORAGE_URL,
-        port: "",
-        pathname: "/profileImage/**"
-      },
-      {
-        protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_SUPABASE_STORAGE_URL,
-        port: "",
-        pathname: "/community/**"
-      },
-      {
-        protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_SUPABASE_STORAGE_URL,
-        port: "",
-        pathname: "/badge/**"
-      },
-      {
+const withImages = require('next-images');
+const withVideos = require('next-videos');
+
+module.exports = withImages(
+  withVideos({
+    reactStrictMode: true,
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: process.env.NEXT_PUBLIC_SUPABASE_STORAGE_URL,
+          port: '',
+          pathname: '/profileImage/**',
+        },
+        {
+          protocol: 'https',
+          hostname: process.env.NEXT_PUBLIC_SUPABASE_STORAGE_URL,
+          port: '',
+          pathname: '/community/**',
+        },
+        {
+          protocol: 'https',
+          hostname: process.env.NEXT_PUBLIC_SUPABASE_STORAGE_URL,
+          port: '',
+          pathname: '/badge/**',
+        },
+        {
         protocol: 'https',
         hostname: 'img.freepik.com',
         port: '',
         pathname: '/premium-photo/**'
       }
-    ]
-  },
-}
-module.exports = nextConfig;
+      ],
+    },
+  })
+);
+
+// issue handling ref : https://stackoverflow.com/questions/60341151/next-images-and-next-videos-are-not-working-properly
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: process.env.NEXT_PUBLIC_SUPABASE_STORAGE_URL,
+//         port: "",
+//         pathname: "/profileImage/**"
+//       },
+//       {
+//         protocol: "https",
+//         hostname: process.env.NEXT_PUBLIC_SUPABASE_STORAGE_URL,
+//         port: "",
+//         pathname: "/community/**"
+//       },
+//       {
+//         protocol: "https",
+//         hostname: process.env.NEXT_PUBLIC_SUPABASE_STORAGE_URL,
+//         port: "",
+//         pathname: "/badge/**"
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'img.freepik.com',
+//         port: '',
+//         pathname: '/premium-photo/**'
+//       }
+//     ]
+//   },
+// }
+// // module.exports = nextConfig;
 
 
-// video
-const withVideos = require("next-videos");
-module.exports = withVideos();
+// // video
+// const withVideos = require("next-videos");
+// const withImages = require("next-images");
+// module.exports = withImages(withVideos())
